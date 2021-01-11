1df71a4ba687dc7cdc8eb4cbaf4d1d5a7c0acf24
owivans <ivan.diaz@nowports.com>
2021-01-11 11:51:11 -0600

    feat(structure): Add project strucutura

---
+++ actions/users.js
@@ -0,0 +1,62 @@
+const mongoose = require('mongoose');
+const User = require('../models/user');
+
+
+exports.get = function (id, callback) {
+    if (!id) {
+        return callback(new Error('Invalid user id'));
+    }
+
+    User.findById(id, function (err, result) {
+        if (err) {
+            return callback(err);
+        }
+        return callback(null, result);
+    });
+}
+
+exports.delete = function (id) {
+    if (!id) {
+        return Promise.reject(new Error('Invalid id'));
+    }
+
+    return User.remove({
+        _id: id
+    });
+}
+
+exports.create = function (data) {
+    if (!data || !data.email || !data.name) {
+        return Promise.reject(new Error('Invalid arguments'));
+    }
+
+    var user = new User(data);
+
+
+    return user.save().then((result) => {
+        return mailer.sendWelcomeEmail(data.email, data.name).then(() => {
+            return {
+                message: 'User created',
+                userId: result.id
+            };
+        });
+    }).catch((err) => {
+        return Promise.reject(err);
+    });
+}
+
+exports.update = async function (id, data) {
+    try {
+        var user = await User.findById(id);
+
+        for (var prop in data) {
+            user[prop] = data[prop];
+        }
+
+        var result = await user.save();
+
+        return result;
+    } catch (err) {
+        return Promise.reject(err);
+    }
+}
